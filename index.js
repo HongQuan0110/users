e142c5772083f2c6a932cfebf49077bc008955d3
HongQuan0110 <lvhquan0110@gmail.com>
2019-02-08 01:21:46 +0700

    Environment variable

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+console.log(process.env.SESSION_SECRET);
 
 var express = require('express');
 var port  = 3000;
@@ -14,7 +17,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser('sghwuihgbjbalj123'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.use(express.static('public'))
 
