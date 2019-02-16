a440869c3b5d1507341884648bdf4e2a054196da
HongQuan0110 <lvhquan0110@gmail.com>
2019-02-17 00:37:11 +0700

    Mongodb

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,26 +1,38 @@
 var db = require('../db.js');
+var Product = require('../models/product.model.js');
 
-module.exports.getProduct = function(req, res){
-    var seesionId = req.signedCookies.sessionId;
+module.exports.getProduct = async function(req, res){
+    // var seesionId = req.signedCookies.sessionId;
 
-    var page = parseInt(req.query.page) || 1;
-    var perPage = 8;
+    // var page = parseInt(req.query.page) || 1;
+    // var perPage = 8;
 
-    var start = (page - 1) * perPage;
-    var end = page * perPage;
+    // var start = (page - 1) * perPage;
+    // var end = page * perPage;
 
-    //var products = db.get('products').value().slice(start, end);
-    var products = db.get('products').take(end).drop(start).value(); // lodash
+    // //var products = db.get('products').value().slice(start, end);
+    // var products = db.get('products').take(end).drop(start).value(); // lodash
 
-    var cart = db.get('sessions').find({id: seesionId}).value().cart;
-    var number = 0;
-    for(var key in cart){
-        number += cart[key];
-    }
+    // var cart = db.get('sessions').find({id: seesionId}).value().cart;
+    // var number = 0;
+    // for(var key in cart){
+    //     number += cart[key];
+    // }
 
+    // res.render('products/index.pug', {
+    //     products: products,
+    //     page: page,
+    //     number: number
+    // });
+
+    // Product.find().then(function(products){
+    //     res.render('products/index.pug', {
+    //         products: products    
+    //     })
+    // })
+
+    var products = await Product.find();
     res.render('products/index.pug', {
-        products: products,
-        page: page,
-        number: number
-    });
+        products: products    
+    })
 }
